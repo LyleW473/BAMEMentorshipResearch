327805613fe69c42afe1f6996927e426adf7e17f
Lyle Wong <wlyle473@gmail.com>
2024-03-08 23:04:02 +0000

    Added cleaning of answers.

--- count_results.py
+++ count_results.py
@@ -1,19 +1,24 @@
 import json
 def count_results(prob_number):
 
-        with open(f"problems/answers/prob{prob_number}_answers.txt", "r") as answers_file:
-            answers = answers_file.readlines()
-            
+    with open(f"problems/answers/prob{prob_number}_answers.txt", "r") as answers_file:
+        answers = answers_file.readlines()
 
-        print(len(answers))
-        print(answers)
-        answers = [answer.replace("\n", "") for answer in answers]
-        print(answers)
+    answers = [answer.replace("\n", "") for answer in answers] # Clean answers
+    answers_indexes = {i: answers[i] for i in range(1, len(answers))} # Indexes for each subquestion, skip the '#' operator
+    print(answers_indexes)
 
-        with open(f"results/prob{prob_number}_predictions.json", "r") as predictions_file:
-            predictions = json.load(predictions_file)
+    print(len(answers))
+    print(answers)
+    print(answers)
+
+    with open(f"results/prob{prob_number}_predictions.json", "r") as predictions_file:
+        predictions = json.load(predictions_file)
+
+    print(predictions)
+    for permutation_string, answer in predictions.items():
+        pass
         
-        print(predictions)
 
 
 
